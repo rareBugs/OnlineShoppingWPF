ea26558dd03130f92e60522bd2b645dde2d7ab36
Mohammed Bekler <mohammed.bekler@outlook.com>
2023-12-11 11:15:09 +0100

    --

---
+++ OnlineShoppingWPF/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace OnlineShoppingWPF
+{
+    class Product: ProductManagmentWindow
+    {
+        List<Product> products = new List<Product>();
+
+       /* Product product = new Product("Alpha Industries", 101, 4500, 350, "Jacket");
+        Product product2 = new Product("Dickies", 101, 4500, 350, "Sweater");
+        Product product3 = new Product("Disel", 101, 4500, 350, "Pants");*/
+
+        public Product(string productName, int productId, int productPrice, int productQuantity, string productCategory)
+        {
+            Name = productName;
+            Id = productId;
+            Price = productPrice;
+            Quantity = productQuantity;
+            Category = productCategory;
+        }
+
+        public string Name {  get; set; }
+        public int Id { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+
+    }
+}
