6277a94c933f76378e9d5d8f6e7911f449947a32
Mohammed Bekler <mohammed.bekler@outlook.com>
2023-12-11 10:59:19 +0100

    Deleted product class.

--- OnlineShoppingWPF/obj/Debug/net6.0-windows/App.g.cs
+++ OnlineShoppingWPF/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace OnlineShoppingWPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace OnlineShoppingWPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public static void Main() {
             OnlineShoppingWPF.App app = new OnlineShoppingWPF.App();
             app.InitializeComponent();
