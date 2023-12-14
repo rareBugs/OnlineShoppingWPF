a777aecaf18f9e4dddcdb630d57833cafb4c27d0
rareBugs <mohammed.bekler@outlook.com>
2023-12-14 18:46:37 +0100

    Stage

--- OnlineShoppingWPF/Product.cs
+++ OnlineShoppingWPF/Product.cs
@@ -10,10 +10,6 @@ namespace OnlineShoppingWPF
 {
     public class Product
     {
-       /* Product product = new Product("Alpha Industries", 101, 4500, 350, "Jacket");
-        Product product2 = new Product("Dickies", 101, 4500, 350, "Sweater");
-        Product product3 = new Product("Disel", 101, 4500, 350, "Pants");*/
-
         public Product(string productName, int productId, int productPrice, int productQuantity, string productCategory)
         {
             Name = productName;
